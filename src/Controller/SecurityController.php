25201287c501fb85fb54425aac82e9b7a28b7bf3
Zyxcel314 <thomasmattone83@outlook.fr>
2019-10-12 20:04:37 +0200

    Add function registration() to Security
    + registration.html.twig

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -2,18 +2,24 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use App\Form\RegistrationType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
 {
     /**
-     * @Route("/security", name="security")
-     */
-    public function index()
-    {
-        return $this->render('security/index.html.twig', [
-            'controller_name' => 'SecurityController',
-        ]);
+     * @Route("/inscription", name="securityRegistration"
+)     */
+    public function registration() {
+        $user = new User();
+        // Formulaire lié au champs de User
+        $form = $this->createForm(RegistrationType::class, $user);
+        return $this->render(
+            'security/registration.html.twig', [
+                'form'=>$form->createView()
+            ]
+            );
     }
 }
