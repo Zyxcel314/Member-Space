e7c93947de41f57881a8d9fd0c5e7067e26039d6
Adam <adam.megnai@gmail.com>
2019-12-19 16:59:26 +0100

    LOAD LES FIXTURES = superadmin mdp : superadmin

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Gestionnaires;
 use App\Entity\InformationMajeur;
 use App\Entity\InformationsMineur;
 use App\Entity\MembreFamille;
@@ -9,40 +10,61 @@ use App\Entity\RepresentantFamille;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
+use App\Entity\Droits;
+
+use App\Repository\RepresentantFamilleRepository;
+use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use Symfony\Bridge\Doctrine\RegistryInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Core\Exception\InvalidCsrfTokenException;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Validator\Constraints\Date;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\FormError;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
+use Symfony\Component\Validator\Constraints\DateTime;
+use Twig\Environment;
+use App\Entity\InformationsFamille;
+use App\Form\RepresentantFamilleType;
+use App\Form\InformationFamilleType;
+
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager)
     {
-        $this->loadRepresentantsFamilles($manager);
-        $this->loadMembresFamilles($manager);
+        //$this->loadRepresentantsFamilles($manager);
+        //$this->loadMembresFamilles($manager);
         // $this->loadDroits($manager);
+
+        $this->loadSuperAdmin($manager);
     }
 
-    public function loadRepresentantsFamilles(ObjectManager $manager)
+
+
+        public function loadSuperAdmin(ObjectManager $manager)
     {
         $rf = [
-          ['id' => 1, 'login' => 'Thomas', 'motdepasse' => '123456', 'nom' => 'Mattone', 'prenom' => 'Thomas'
-          , 'adresse' => 'Belfort', 'nomobile' => '0707070707', 'nofixe' => '0494777777', 'mail' => 'thomas@mattone'
-          , 'datenaissance' => '2000-01-06', 'datefinadhesion' => '2019-12-02'],
-          ['id' => 2, 'login' => 'Zoe', 'motdepasse' => '123456', 'nom' => 'Siena', 'prenom' => 'Zoe'
-            , 'adresse' => 'Hyeres', 'nomobile' => '0606060606', 'nofixe' => '0494666666', 'mail' => 'zoe@siena'
-            , 'datenaissance' => '2002-07-02', 'datefinadhesion' => '2020-02-02']
+            ['id' => 1, 'nom' => 'superadmin', 'prenom' => 'superadmin', 'motdepasse' => '$2y$13$FiOcaFSVSpAKWIFbRUdt5e0vZc66iksotns/0hhGZ.660vth9m1a6'],
+
+
         ];
 
+
         foreach ($rf as $r) {
-            $new_rf = new RepresentantFamille();
-            $new_rf->setLogin($r['login']);
-            $new_rf->setMotdepasse($r['motdepasse']);
+            $new_rf = new Gestionnaires();
+            $gestionnaire = new Gestionnaires();
             $new_rf->setNom($r['nom']);
             $new_rf->setPrenom($r['prenom']);
-            $new_rf->setAdresse($r['adresse']);
-            $new_rf->setNoMobile($r['nomobile']);
-            $new_rf->setNoFixe($r['nofixe']);
-            $new_rf->setMail($r['mail']);
-            $new_rf->setDateNaissance(new \DateTime($r['datenaissance']));
-            $new_rf->setDateFinAdhesion(new \DateTime($r['datefinadhesion']));
-            $new_rf->setEstActive(1);
-            $new_rf->setMailTokenVerification('1234');
+            $new_rf->setMotdepasse($r['motdepasse']);
             $manager->persist($new_rf);
             $manager->flush();
         }
